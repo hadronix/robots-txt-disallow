b82a51c8cc19f320dbda7227d3a0e24637e708f5
Rudi Diedrich <info@rudidiedrich.de>
2025-11-19 11:07:06 +0100

    init commit

---
+++ robots-txt-disallow.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name:  Disallow Robots.txt Indexing
+Plugin URI:   https://rudidiedrich.de/
+Description:  Disallow indexing at all
+Version:      0.0.1
+Author:       Rudi Diedrich
+Author URI:   https://rudidiedrich.de/
+Text Domain:  rd
+License:      MIT License
+*/
+
+if (!defined('DISALLOW_INDEXING') || DISALLOW_INDEXING !== true) {
+    return;
+}
+
+add_filter('robots_txt', function ($output) {
+    $public = (bool) get_option( 'blog_public' );
+    if ( $public ) {
+        return $output;
+    }
+
+    $output = "User-agent: *\n";
+    $output .= "Disallow: /\n";
+    return $output;
+}, PHP_INT_MAX );
